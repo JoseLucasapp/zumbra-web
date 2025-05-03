5165650504422586c16b88b5095316fd08f9302e
José Lucas <josluc2016@gmail.com>
2025-05-03 13:43:13 -0300

    fix: mobile view

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,25 @@
+import { useEffect, useState } from 'react'
 import './style.css'
 
 const Home = () => {
-    return (
-        <section className="main">
-            <div className="topLeft">
-                <p>Enter the wild side of<br />programming.</p>
-            </div>
 
-            <div className="credits">
-                <p>Photo by Magda Ehlers</p>
-            </div>
+    const [isMobile, setMobile] = useState(false)
+    const [isMenuOpen, setMenuOpen] = useState(false)
 
-            <div className="welcome">
-                <p>Welcome to</p>
-            </div>
+    useEffect(() => {
+        handleISMobile()
+    }, [])
+    const handleISMobile = () => {
+        if (window.innerWidth < 800) {
+            setMobile(true)
+        } else {
+            setMobile(false)
+        }
+    }
 
-            <div className="zumbra">
-                <p>ZUMBRA</p>
-            </div>
-
-            <div className="menu">
+    const handleMenu = () => {
+        return (
+            <div className='menu-modal'>
                 <nav>
                     <ul>
                         <a href="/about"><li>About</li></a>
@@ -30,6 +30,77 @@ const Home = () => {
                     </ul>
                 </nav>
             </div>
+        )
+    }
+
+    const menu = () => {
+        return (
+            <div className="menu-bar-icon" onClick={() => setMenuOpen(!isMenuOpen)}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        )
+    }
+    return (
+        <section className="main">
+            <div className="topLeft">
+                <p>Enter the wild side of<br />programming.</p>
+            </div>
+
+            <div className="credits">
+                {
+                    isMobile && (
+                        <p>Photo by Taryn Elliott</p>
+                    )
+                }
+                {
+                    !isMobile && (
+                        <p>Photo by Magda Ehlers</p>
+                    )
+                }
+
+            </div>
+
+            <div className="welcome">
+                <p>Welcome to</p>
+            </div>
+
+            <div className="zumbra">
+                <p>ZUMBRA</p>
+            </div>
+
+            {
+                isMobile && (
+                    <nav>
+                        {menu()}
+                    </nav>
+                )
+            }
+
+            {
+                !isMobile && (
+                    <div className="menu">
+                        <nav>
+                            <ul>
+                                <a href="/about"><li>About</li></a>
+                                <a href="/gallery"><li>Gallery</li></a>
+                                <a href="/docs"><li>Docs</li></a>
+                                <a href="https://github.com/JoseLucasapp/Zumbra-lang"><li>Repository</li></a>
+                                <a href="https://github.com/JoseLucasapp/Zumbra-lang/releases"><li>Download</li></a>
+                            </ul>
+                        </nav>
+                    </div>
+                )
+            }
+
+            {
+                isMenuOpen && (
+                    <nav>
+                        {handleMenu()}
+                    </nav>
+                )
+            }
         </section>
     )
 }
